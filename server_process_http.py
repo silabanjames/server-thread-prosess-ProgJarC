3553d01dcb601c4b52f481a9244fb963e1277a2f
silabanjames <78299006+silabanjames@users.noreply.github.com>
2023-04-01 23:58:14 +0700

    edit listen

--- server_process_http.py
+++ server_process_http.py
@@ -56,7 +56,7 @@ class Server(multiprocessing.Process):
 
 	def run(self):
 		self.my_socket.bind(('0.0.0.0', 8889))
-		self.my_socket.listen(1)
+		self.my_socket.listen(100)
 		while True:
 			self.connection, self.client_address = self.my_socket.accept()
 			logging.warning("connection from {}".format(self.client_address))
